57f9a4ed5da8169109a1e46be876824bb99d49f6
jputro <36195130+jputro@users.noreply.github.com>
2018-05-11 16:38:20 +0300

    Create run.sh
    Configure Live USB

---
+++ run.sh
@@ -0,0 +1,19 @@
+echo "Mern-module Live USB configuration Starting... jputro/mern-module"
+set -o verbose
+
+echo "Installing Salt and Git"
+sudo apt-get update
+sudo apt-get -y install git salt-minion
+
+echo "Retrieving and Applying Settings"
+echo 'master: localhost'|sudo tee /etc/salt/minion
+git clone https://github.com/jputro/mern-module
+cd mern-module/
+./highstate.sh
+
+echo "Configuring Live Desktop"
+setxkbmap fi
+sudo timedatectl set-timezone Europe/Helsinki
+
+echo "More tips on http://jaanaputro.wordpress.com"
+echo "Done."
